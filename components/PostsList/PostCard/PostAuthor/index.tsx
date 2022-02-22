fb3c9ed787545103be72f6e5bc935082afa74d02
Krb <maurodavidcaceres@gmail.com>
2022-02-22 15:26:28 -0300

    refactoring

---
+++ components/PostsList/PostCard/PostAuthor/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import CalendarIcon from '@heroicons/react/outline/CalendarIcon'
+import moment from 'moment'
+import Post from '../../../interfaces/Post'
+
+interface IProps {
+  post: Post
+}
+
+const PostAuthor = (props: IProps) => {
+  const { post } = props
+  return (
+    <div className="mb-8 block w-full items-center justify-center text-center lg:flex">
+      <div className="mb-4 mr-8 flex w-full items-center justify-center lg:mb-0 lg:w-auto">
+        <img
+          alt={post.author.name}
+          height="25px"
+          width="25px"
+          className="rounded-full align-middle"
+          src={post.author.photo.url}
+        />
+        <p className="ml-2 inline align-middle text-lg font-medium text-gray-700">
+          {post.author.name}
+        </p>
+      </div>
+      <div className="font-medium text-gray-700">
+        <CalendarIcon className="mr-2 inline w-6 text-pink-600" />
+        <span className="align-middle">
+          {moment(post.createdAt).format('MMM D, YYYY')}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default PostAuthor
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-import Categories from '../components/Categories'
-import PostCard from '../components/PostCard'
-import PostWidget from '../components/PostCard/PostWidget'
-import { Post } from '../interfaces/Posts'
+import Aside from '../components/Aside'
+import PostsList from '../components/PostsList'
+import { Posts } from '../interfaces/Post'
 import { getPosts } from '../services'
 
 interface IProps {
-  posts: Post[]
+  posts: Posts
 }
 export default function Home({ posts }: IProps) {
   return (
@@ -16,17 +15,8 @@ export default function Home({ posts }: IProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        <div className="col-span-1 lg:col-span-8 ">
-          {posts.map((post: Post) => (
-            <PostCard key={post.title} post={post} />
-          ))}
-        </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative top-8 lg:sticky">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
+        <PostsList posts={posts} />
+        <Aside />
       </div>
     </div>
   )
