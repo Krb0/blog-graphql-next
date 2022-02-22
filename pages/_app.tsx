04ebf40d7ae0013aeabc9779828fc1bee15bb0f5
Krb <maurodavidcaceres@gmail.com>
2022-02-22 15:26:08 -0300

    Layout added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.scss'
 import React, { useEffect, useState } from 'react'
-import Layout from '../components/Layout'
+import Layout from '../Layout'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
