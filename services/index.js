6319a9dc3fe2efde371466907d80fe4da3ff2740
Krb <maurodavidcaceres@gmail.com>
2022-02-22 15:26:03 -0300

    getCategories added

--- services/index.js
+++ services/index.js
@@ -30,6 +30,74 @@ export const getPosts = async () => {
     }
   `
 
+  const res = await request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, query)
+  const result = res.postsConnection.edges.map(({ node }) => {
+    return { ...node }
+  })
+  return result
+}
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const res = await request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, query)
+  const result = res.posts.map((post) => {
+    return { ...post }
+  })
+  console.log(result)
+  return result
+}
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const result = await request(
+    process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+    query,
+    { slug, categories }
+  )
+
+  return result.posts
+}
+
+export const getCategories = async (categories) => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `
   const result = await request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, query)
-  return result.postsConnection.edges
+  return result.categories
 }
