1191716879e089867ce6a98531bb6547dcae88e7
Krb <maurodavidcaceres@gmail.com>
2022-02-23 05:21:31 -0300

    all posts endpoint created

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,3 +1,38 @@
-export default function handler(req, res) {
-  res.status(200).json(people)
+import { request, gql } from 'graphql-request'
+export default async function Posts(req, res) {
+  const query = gql`
+    query PostsQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const posts = await request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, query)
+  const result = posts.postsConnection.edges.map(({ node }) => {
+    return { ...node }
+  })
+  res.status(200).json(result)
 }
