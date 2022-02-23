c08c8710d398abf54cb82a2284e5fa0adad04f90
Krb <maurodavidcaceres@gmail.com>
2022-02-23 02:42:57 -0300

    Post section added

---
+++ components/Post/Comments/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommentsList from './CommentsList'
+import CommentsForm from './CommentsForm'
+const Comments = () => {
+  return (
+    <div>
+      <CommentsList />
+      <CommentsForm />
+    </div>
+  )
+}
+
+export default Comments
---
+++ components/Post/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Author from './Author'
+import Comments from './Comments'
+import PostDetails from './PostDetails'
+const index = () => {
+  return (
+    <>
+      <PostDetails />
+      <Author />
+      <Comments />
+    </>
+  )
+}
+
+export default index
