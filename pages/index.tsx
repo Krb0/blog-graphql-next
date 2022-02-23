6bf2fa9295e20e8a92acbc98060f02b75839b3be
Krb <maurodavidcaceres@gmail.com>
2022-02-23 02:44:17 -0300

    refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,20 @@
 import Head from 'next/head'
-import Aside from '../components/Aside'
-import PostsList from '../components/PostsList'
+import HomeSection from '../components/Home'
 import { Posts } from '../interfaces/Post'
 import { getPosts } from '../services'
+import { NextPage } from 'next'
 
 interface IProps {
   posts: Posts
 }
-export default function Home({ posts }: IProps) {
+const Home: NextPage<IProps> = ({ posts }) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        <PostsList posts={posts} />
-        <Aside />
-      </div>
+      <HomeSection posts={posts} />
     </div>
   )
 }
@@ -30,3 +27,5 @@ export async function getStaticProps() {
     },
   }
 }
+
+export default Home
