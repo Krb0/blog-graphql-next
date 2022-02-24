bfd9e84d0916bd3a7cef1a8f1817ca25af7e94be
Krb <maurodavidcaceres@gmail.com>
2022-02-24 14:53:14 -0300

    refactoring

--- components/Post/Author/index.tsx
+++ components/Post/Author/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import Author from '../../../interfaces/Author'
+import IAuthor from '../../../interfaces/Author'
+import Image from 'next/image'
 
 interface IProps {
-  author: Author
+  author: IAuthor
 }
 
 const Author = (props: IProps) => {
-  return <div>Author</div>
+  const { author } = props
+  return (
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          alt={author.name}
+          unoptimized
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url} 
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio} </p>
+    </div>
+  )
 }
 
 export default Author
