b5440235b680ba04840248cab99b47dc3b887ee0
Krb <maurodavidcaceres@gmail.com>
2022-02-24 18:42:49 -0300

    Head added

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -6,6 +6,7 @@ import Categories from '../../components/Aside/Categories'
 import PostWidget from '../../components/Aside/PostWidget'
 import Post from '../../interfaces/Post'
 import Layout from '../../Layout'
+import Head from 'next/head'
 
 interface IProps {
   postRes: Post
@@ -24,24 +25,30 @@ const Post: NextPage | {} = (props: IProps) => {
 
   return (
     <Layout>
-      <div className="container mx-auto mb-8 px-10">
-        <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-          <div className="col-span-1 lg:col-span-8">
-            <PostDetails post={singlePost} />
-          </div>
-          <div className="col-span-1 lg:col-span-4">
-            <div className="relative top-8 lg:sticky">
-              <PostWidget
-                slug={slug}
-                categories={singlePost?.categories?.map(
-                  (category) => category.slug
-                )}
-              />
-              <Categories />
+      <>
+        <Head>
+          <title>{singlePost.title} | GraphBlog</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="container mx-auto mb-8 px-10">
+          <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+            <div className="col-span-1 lg:col-span-8">
+              <PostDetails post={singlePost} />
+            </div>
+            <div className="col-span-1 lg:col-span-4">
+              <div className="relative top-8 lg:sticky">
+                <PostWidget
+                  slug={slug}
+                  categories={singlePost?.categories?.map(
+                    (category) => category.slug
+                  )}
+                />
+                <Categories />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     </Layout>
   )
 }
