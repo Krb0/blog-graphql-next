0598b5e46bf64b7b5055e40808b98fa713c595c8
Krb <maurodavidcaceres@gmail.com>
2022-02-26 13:44:50 -0300

    Comments list added

--- components/Post/Comments/CommentsList/index.tsx
+++ components/Post/Comments/CommentsList/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Comment, { Comments } from '../../../../interfaces/Comment'
+import moment from 'moment'
+import parse from 'html-react-parser'
 interface IProps {
   slug: string
 }
 
 const CommentsList = ({ slug }: IProps) => {
-  return <div>CommentsList</div>
+  const [comments, setComments] = useState<Comments | []>([])
+
+  useEffect(() => {
+    fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ slug: slug }),
+    })
+  }, [])
+  return (
+    <div>
+      {comments.map(({ comment }) => {
+        ;<h1>{comment}</h1>
+      })}
+    </div>
+  )
 }
 
 export default CommentsList
