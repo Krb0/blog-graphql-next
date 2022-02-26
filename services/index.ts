422d8d42e86ae8f8621f979422abfe888b995003
Krb <maurodavidcaceres@gmail.com>
2022-02-26 17:17:45 -0300

    refactoring

--- services/index.ts
+++ services/index.ts
@@ -1,4 +1,9 @@
-export const submitComment = async (obj) => {
+export const submitComment = async (obj: {
+  name: string
+  email: string
+  comment: string
+  slug: string
+}) => {
   const result = await fetch('/api/postcomment', {
     method: 'POST',
     body: JSON.stringify(obj),
