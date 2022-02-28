91ae76e03e11a4a9b8dc4ba0521a69637ff57d65
Krb <maurodavidcaceres@gmail.com>
2022-02-28 14:51:21 -0300

    comment interface added

--- interfaces/Comment.ts
+++ interfaces/Comment.ts
@@ -4,10 +4,11 @@ export default interface Comment {
   name: string
   email: string
   comment: string
-  id: string
-  createdAt: string
   image: string
-  post: Post
+  slug: string
+  id?: string
+  createdAt?: string
+  post?: Post
 }
 
 export type Comments = Comment[]
